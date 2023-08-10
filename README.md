1edc91797a3e0e12b86b2e17a4915425e8020ba5
Sergio Redondo <rodo1985@gmail.com>
2023-08-10 16:54:19 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # luxonis_oakd_examples
 List of own oakd test and examples
+
+## Installation
+Install requirements:
+
+```bash
+pip3 install -r requirements.txt
+```
+
+## Examples
+*   **save_rgb_images.py** -> Example of image RGB preview and how to save images in a folder. It is useful to dataset collection.
---
+++ save_rgb_images.py
@@ -0,0 +1,60 @@
+import datetime
+import os
+import cv2
+import depthai as dai
+
+# output folders
+out_folder = 'images'
+
+# if output folder does not exist, create it
+if not os.path.exists(out_folder):
+    os.makedirs(out_folder)
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define source and output
+camRgb = pipeline.create(dai.node.ColorCamera)
+xoutRgb = pipeline.create(dai.node.XLinkOut)
+
+xoutRgb.setStreamName("rgb")
+
+# Properties
+camRgb.setPreviewSize(1080, 720)
+camRgb.setInterleaved(False)
+camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+
+# Linking
+camRgb.preview.link(xoutRgb.input)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+
+    print('Connected cameras:', device.getConnectedCameraFeatures())
+    # Print out usb speed
+    print('Usb speed:', device.getUsbSpeed().name)
+    # Bootloader version
+    if device.getBootloaderVersion() is not None:
+        print('Bootloader version:', device.getBootloaderVersion())
+    # Device name
+    print('Device name:', device.getDeviceName())
+
+    # Output queue will be used to get the rgb frames from the output defined above
+    qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+
+    while True:
+        inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
+
+        image = inRgb.getCvFrame()
+
+        # Retrieve 'bgr' (opencv format) frame
+        cv2.imshow("rgb", image)
+
+        # get key pressed
+        key = cv2.waitKey(1)
+
+        if key == ord('q'):
+            break
+        elif key == ord('s'):
+            # save image in png with datetime as filename
+            cv2.imwrite(os.path.join(out_folder, datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + '.png'), image)
